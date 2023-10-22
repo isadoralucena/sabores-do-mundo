a0ab1489985aaeb504e8f5554de2231f30362bf2
Isadora Lucena <66965789+Isadoralucena@users.noreply.github.com>
2023-10-21 21:02:38 -0300

    add interactive map, header and footer components, additions in mexico

---
+++ components/header.js
@@ -0,0 +1,37 @@
+class Header extends HTMLElement {
+    constructor() {
+        super()
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+        <!-- ======= Header ======= -->
+        <header id="header" class="fixed-top d-flex align-items-center header-transparent">
+            <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+            <div class="logo me-auto">
+                <!-- Uncomment below if you prefer to use an image logo -->
+                <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
+                <h1 class="align-middle"><a href="index.html">Sabores do Mundo</a></h1>
+            </div>
+            <nav id="navbar" class="navbar order-last order-lg-0">
+                <ul>
+                <li><a class="nav-link scrollto" href="index.html">Página inicial</a></li>
+                <li><a class="nav-link scrollto" href="index.html #map">Continentes</a></li>
+                <li><a class="nav-link scrollto" href="index.html #chefs">Equipe</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+            </div>
+        </header><!-- End Header -->
+        `
+    }
+
+    disconnectedCallback() {
+        console.log('Dismounted', this);
+    }
+}
+
+if ('customElements' in window) {
+    customElements.define('main-header', Header)
+}
+  
