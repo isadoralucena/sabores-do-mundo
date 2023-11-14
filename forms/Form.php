646c38e1a72d62b786da74ba4b3fa8125e07a40d
Isadora Lucena <66965789+Isadoralucena@users.noreply.github.com>
2023-11-13 23:01:34 -0300

    final form

--- forms/Form.php
+++ forms/Form.php
@@ -5,21 +5,66 @@ include __DIR__ . '/Model.php';
 class Form extends Model
 {
    public function __construct(
-        private $objective,
-        private $visual_identity,
+        private $age,
+        private $clear_objective,
+        private $understanding_objective,
+        private $culinary_diversity,
+        private $understandable_language,
+        private $proposal,
+        private $pleasant_visual_identity,
+        private $culinary_visual_identity,
+        private $color_pattern,
+        private $coherent_logo,
+        private $suggestion
     ) {}
 
     public function save()
     {
         $statement = self::$connection->prepare(
-            "INSERT INTO forms (objective, visual_identity) VALUES (:objective, :visual_identity);");
+            "INSERT INTO forms (age, clear_objective, understanding_objective, culinary_diversity, understandable_language, proposal, pleasant_visual_identity, culinary_visual_identity, color_pattern, coherent_logo, suggestion) VALUES (:age, :clear_objective, :understanding_objective, :culinary_diversity, :understandable_language, :proposal, :pleasant_visual_identity, :culinary_visual_identity, :color_pattern, :coherent_logo, :suggestion);");
 
         $statement->bindValue(
-            ':objective', $this->objective, SQLITE3_TEXT
+            ':age', $this->age, SQLITE3_TEXT
         );
 
         $statement->bindValue(
-            ':visual_identity', $this->visual_identity, SQLITE3_TEXT
+            ':clear_objective', $this->clear_objective, SQLITE3_TEXT
+        );
+
+        $statement->bindValue(
+            ':understanding_objective', $this->understanding_objective, SQLITE3_TEXT
+        );
+
+        $statement->bindValue(
+            ':culinary_diversity', $this->culinary_diversity, SQLITE3_TEXT
+        );
+
+        $statement->bindValue(
+            ':understandable_language', $this->understandable_language, SQLITE3_TEXT
+        );
+
+        $statement->bindValue(
+            ':proposal', $this->proposal, SQLITE3_TEXT
+        );
+
+        $statement->bindValue(
+            ':pleasant_visual_identity', $this->pleasant_visual_identity, SQLITE3_TEXT
+        );
+
+        $statement->bindValue(
+            ':culinary_visual_identity', $this->culinary_visual_identity, SQLITE3_TEXT
+        );
+
+        $statement->bindValue(
+            ':color_pattern', $this->color_pattern, SQLITE3_TEXT
+        );
+
+        $statement->bindValue(
+            ':coherent_logo', $this->coherent_logo, SQLITE3_TEXT
+        );
+
+        $statement->bindValue(
+            ':suggestion', $this->suggestion, SQLITE3_TEXT
         );
 
         return $statement->execute();
--- forms/save.php
+++ forms/save.php
@@ -8,22 +8,39 @@ Model::createModel(
     'CREATE TABLE IF NOT EXISTS forms
     (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        objective TEXT,
-        visual_identity TEXT
+        age TEXT,
+        clear_objective TEXT,
+        understanding_objective TEXT,
+        culinary_diversity TEXT,
+        understandable_language TEXT,
+        proposal TEXT,
+        pleasant_visual_identity TEXT,
+        culinary_visual_identity TEXT,
+        color_pattern TEXT,
+        coherent_logo TEXT,
+        suggestion TEXT
     );'
 );
 
 if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (isset($_POST['objective'])) {
-        $form = new Form(
-            $_POST['objective'],
-            $_POST['visual_identity']
-        );
+    $form = new Form(
+        $_POST['age'],
+        $_POST['clear_objective'],
+        $_POST['understanding_objective'],
+        $_POST['culinary_diversity'],
+        $_POST['understandable_language'],
+        $_POST['proposal'],
+        $_POST['pleasant_visual_identity'],
+        $_POST['culinary_visual_identity'],
+        $_POST['color_pattern'],
+        $_POST['coherent_logo'],
+        $_POST['suggestion']
+    );
 
-        $form->save();
+    $form->save();
 
-        header('Location: /index.html', true, 302);
+    header('Location: /index.html', true, 302);
 
-        exit;
-    }
+    exit;
+
 }
